370ed48e40198a8f77f11ecd4b19728bbad7485f
oerdbej <oerdbej@gmail.com>
2025-03-15 13:44:10 +0100

    creating and saving the user for the sign up

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -18,6 +18,20 @@ export const signup = async (req, res) => {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
+    const verificationToken = Math.floor(
+      10000 * Math.random() * 90000
+    ).toString();
+
+    const user = new User({
+      email,
+      password: hashPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, //24 hours
+    });
+
+    //save it to db
+    await user.save();
 
     //user dont exist hash the password
   } catch (error) {}
